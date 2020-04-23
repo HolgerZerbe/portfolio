6346fbd98781448486548107a6b54c020280e9bf
Holger Zerbe <Holger.Zerbe@web.de>
2020-04-23 15:28:57 +0200

    fading style changed to three images

--- code/app/src/components/startPage/StartPage.js
+++ code/app/src/components/startPage/StartPage.js
@@ -6,11 +6,15 @@ import {switchStyle} from '../../actions';
 
 class StartPage extends Component {
 
-    state= {opacity : 0}
+    state= {opacity1 : 1,
+            opacity2 : 0,
+            opacity3 : 0}
+
     counter = 0;
     opacityIntervall = 0;
-    counterSummand = 0;
-    opacitySummand = 0;
+    opacitySummand1 = 0;
+    opacitySummand2 = 0;
+    opacitySummand3 = 0;
 
     changeStyle = (linkstyle, headerstyle, navbarstyle) => {
         localStorage.setItem("style", JSON.stringify([linkstyle, headerstyle, navbarstyle]));
@@ -18,24 +22,35 @@ class StartPage extends Component {
     }
 
     changeOpacity = () => {
-        if (this.counter === 0) { 
-            this.counterSummand = 1;
-            this.opacitySummand = 0.001;
+
+        if (this.counter === 0 || this.counter === 2000 || this.counter === 4000 ) { 
+            this.opacitySummand1 = 0;
+            this.opacitySummand2 = 0;
+            this.opacitySummand3 = 0;
         }
-            else if (this.counter === 1000 || this.counter === 3000)
-            {
-                this.opacitySummand = 0;
-            }
-            else if (this.counter === 2000) {
-                this.opacitySummand = -0.001;
-                this.counterSummand = -1;
-            } 
-            else if (this.counter === 4000) {
-                this.counter = 0;
-            }
-            this.counter = this.counter + this.counterSummand                
-            this.setState({opacity: this.state.opacity + this.opacitySummand})
-            console.log(this.state.opacity)
+        else if (this.counter === 1000){
+            this.opacitySummand1 = -0.001;
+            this.opacitySummand2 = 0.001;
+            this.opacitySummand3 = 0;
+        }
+        else if (this.counter === 3000){
+            this.opacitySummand1 = 0;
+            this.opacitySummand2 = -0.001;
+            this.opacitySummand3 = 0.001;
+        }
+        else if (this.counter === 5000){
+            this.opacitySummand1 = 0.001;
+            this.opacitySummand2 = 0;
+            this.opacitySummand3 = -0.001;
+        }
+
+        this.counter++              
+        this.setState({opacity1: this.state.opacity1 + this.opacitySummand1,
+                    opacity2: this.state.opacity2 + this.opacitySummand2,
+                    opacity3: this.state.opacity3 + this.opacitySummand3})
+        if (this.counter === 6000) {
+            this.counter = 0;
+        }
     }
   
     componentDidMount(){
@@ -52,8 +67,9 @@ class StartPage extends Component {
             <div className="startPage_div">
                 <h2 className="welcome">Willkommen, Bienvenue, Welcome!</h2>
                 <div className="photo">
-                    <img className = "photoStartPage staticPhoto" src="/image/holger.png" alt="Holger Zerbe"></img>
-                    <img className = "photoStartPage fadingPhoto" style={{"opacity": this.state.opacity }} src="/image/HZ-Foehr-3-kopie.jpeg" alt="Holger Zerbe"></img>
+                    <img className = "photoStartPage staticPhoto" style= {{"opacity": this.state.opacity1 }} src="/image/HZ-Logo.jpg" alt="Holger Zerbe Logo"></img>
+                    <img className = "photoStartPage fadingPhoto1" style={{"opacity": this.state.opacity2 }} src="/image/holger.png" alt="Holger Zerbe"></img>
+                    <img className = "photoStartPage fadingPhoto2" style={{"opacity": this.state.opacity3 }} src="/image/holger2.jpeg" alt="H  olger Zerbe"></img>
                     </div>
                 <h2 className="homepage">{this.props.german ? <>auf meiner Portfolio-Seite</> : <>to my portfolio site</>}</h2>
         <h3 className="styleHeadline">{this.props.german ? <>Bitte wähle einen Stil</> : <> Please choose your favourite style</>}</h3>
